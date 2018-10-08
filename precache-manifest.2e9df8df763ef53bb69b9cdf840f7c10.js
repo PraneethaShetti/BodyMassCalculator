bb26cd9cdf3f43e189b9b6f68c9b63e697d7c9dc
PraneethaShetti <pshetti062@gmail.com>
2018-10-08 15:40:37 -0700

    Updates

---
+++ precache-manifest.2e9df8df763ef53bb69b9cdf840f7c10.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "49b95c922dd31047b2c8",
+    "url": "/BodyMassCalculator/static/js/runtime~main.49b95c92.js"
+  },
+  {
+    "revision": "0e453ee11cf23d4e47db",
+    "url": "/BodyMassCalculator/static/js/main.0e453ee1.chunk.js"
+  },
+  {
+    "revision": "462048e097348080ed49",
+    "url": "/BodyMassCalculator/static/js/1.462048e0.chunk.js"
+  },
+  {
+    "revision": "0e453ee11cf23d4e47db",
+    "url": "/BodyMassCalculator/static/css/main.e66d96df.chunk.css"
+  },
+  {
+    "revision": "61558333de330b4a70cea3b984be287d",
+    "url": "/BodyMassCalculator/index.html"
+  }
+];
